d40ee5ca7254fb0ed52f60c11226a8aadaadaec1
FCL00 <flagahit00@gmail.com>
2025-09-01 17:38:38 +0800

    chore: add new tasks

--- backend/src/routes/task.routes.ts
+++ backend/src/routes/task.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { getAllTask, getTaskById, createTask } from "@/controllers/task.controller.js";
+import requireAuth from "@/middleware/requireAuth.middleware.js";
+
 
 const taskRouter = Router()
 
+taskRouter.use(requireAuth)
 taskRouter.get('/', getAllTask)
 taskRouter.get('/:id', getTaskById)
-taskRouter.post('/create', createTask)
+taskRouter.post('/', createTask)
+taskRouter.put('/', createTask)
 
 
 export default taskRouter

db6be8720e9b88dabaf4aa6327ccea6f531a4d0a
FCL00 <flagahit00@gmail.com>
2025-09-02 17:46:03 +0800

    feature: create a controller that handles basic crud for the checklist

---
+++ backend/src/controllers/checklist.controller.ts
@@ -0,0 +1,63 @@
+import type { Request, Response, NextFunction } from "express";
+import { checkListService } from "@/service/checklistService.js";
+import { TaskService } from "@/service/taskService.js";
+import { successResponse, errorResponse } from "@/utils/responseHandler.js";
+
+
+export const createItem = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const taskId = req.params.taskId as string;
+    const validatedInput = req.body;
+    const userId = req.user?.id as string;
+
+    const task = await TaskService.findById(taskId)
+
+    if (!task || task.userId !== userId) {
+      return errorResponse(res, "Task not found or unauthorized", null, 404)
+    }
+
+    const newCheckListItem = await checkListService.create(taskId, validatedInput)
+    return successResponse(res, "Checklist item created successfully", newCheckListItem)
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateItem = async (req: Request, res: Response, next: NextFunction ) => {
+    try { 
+        const { taskId, itemId } = req.params
+        const validatedInput = req.body;
+        const userId = req.user?.id as string;
+
+        const task = await TaskService.findById(taskId as string)
+
+        if(!task || task.userId !== userId ){
+            return errorResponse(res, "Task not found or unauthorized", null, 404)
+        }
+
+        const updateCheckListItem = await checkListService.update(itemId as string, validatedInput)
+        return successResponse(res, "Checklist item updated", updateCheckListItem);
+
+    } catch(error) {
+        next(error)
+    }
+}
+
+
+export const deleteItem = async (req: Request, res: Response, next: NextFunction ) => {
+    try { 
+        const { taskId, itemId } = req.params
+        const userId = req.user?.id;
+        const task = await TaskService.findById(taskId as string)
+
+        if(!task || task.userId !== userId ){
+            return errorResponse(res, "Task not found or unauthorized", null, 404)
+        }
+
+        await checkListService.delete(itemId as string)
+        return successResponse(res, "Checklist item deleted", null);
+    } catch(error) {
+        next(error)
+    }
+}

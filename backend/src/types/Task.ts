99e50ee6848b9eee1215d5848975c007abf87357
FCL00 <flagahit00@gmail.com>
2025-09-02 17:50:21 +0800

    chore: add types for the task and checklist

--- backend/src/types/Task.ts
+++ backend/src/types/Task.ts
@@ -2,7 +2,23 @@ export interface Tasks {
   title: string
   userId: string
   description: string
-  startDate: Date
-  endDate: Date
+  position: number
+  startDate?: Date
+  endDate?: Date
   status: "ongoing" | "completed" | "archive"
+}
+
+export interface ChecklistItem {
+  taskId: string
+  userId: string
+  title: string
+  description: string
+  position: number
+  status: "ongoing" | "completed" | "archive"
+}
+
+export interface Comments {
+  checklistItemId: string
+  userId: string
+  body: string
 }

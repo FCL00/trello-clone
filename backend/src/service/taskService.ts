65ed26e8717ae805ffe16ed17c1e120ae4d08b6b
FCL00 <flagahit00@gmail.com>
2025-09-03 18:00:43 +0800

    chore: update the task service

--- backend/src/service/taskService.ts
+++ backend/src/service/taskService.ts
@@ -1,31 +1,32 @@
-import prisma from "@/config/prisma.js"
+import prisma from "@/config/prisma.js";
 import type { Tasks } from "@/types/Task.js";
 
-
 export const TaskService = {
-    findAll: async (userId: string) => {
-        return await prisma.task.findMany({
-            where: { userId: userId},
-            include: { checklistItem: true },
-            orderBy: { position: "asc"}
-        });
-    },  
+  findAll: async (userId: string) => {
+    return await prisma.task.findMany({
+      where: { userId: userId },
+      include: { checklistItem: true },
+      orderBy: { position: "asc" },
+    });
+  },
 
-    findById: async (id: string) => {
-        return await prisma.task.findUnique({ where: {id: id}})
-    },
+  findById: async (id: string) => {
+    return await prisma.task.findUnique({ where: { id: id } });
+  },
 
+  create: async (task: Tasks) => {
+    return await prisma.task.create({ data: task });
+  },
 
-    create: async (task: Tasks) => {
-        return await prisma.task.create({data: task})
-    },
+  update: async (id: string, task: Partial<Tasks>) => {
+    return await prisma.task.update({
+      where: { id: id },
+      data: task,
+      include: { checklistItem: true },
+    });
+  },
 
-    update: async (id: string, task: Partial<Tasks>) => {
-        return await prisma.task.update({ where: { id: id}, data: task})
-    },
-
-    delete: async (id: string) => {
-        return await prisma.task.delete({ where: {id: id}})
-    },
-
+  delete: async (id: string) => {
+    return await prisma.task.delete({ where: { id: id } });
+  },
 }

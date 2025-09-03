78ad40aaafff999a2951c104ec8a83c22c3cee25
FCL00 <flagahit00@gmail.com>
2025-09-03 18:03:08 +0800

    chore: set the BASE_URL base on .env

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
   withCredentials: true, // always send cookies
 });
 
